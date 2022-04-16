6ec2280487c6f4ec566887118d7b5e130f666a28
Blackjuker <mikarno2@gmail.com>
2022-04-16 10:17:02 +0200

    animations

--- app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
+++ app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
@@ -10,5 +10,6 @@ public class Splash  extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
     }
 }
