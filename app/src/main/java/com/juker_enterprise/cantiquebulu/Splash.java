7d175a1ab76f670a2bc7ba2b1a85969666f66dfb
Blackjuker <mikarno2@gmail.com>
2022-04-16 10:32:21 +0200

    animations page

--- app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
+++ app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
@@ -1,15 +1,33 @@
 package com.juker_enterprise.cantiquebulu;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash  extends AppCompatActivity {
 
+    ImageView img1, img2;
+    Animation top,bottom;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
+
+
+        img1 = (ImageView) findViewById(R.id.logo);
+        img2 = (ImageView) findViewById(R.id.logo2);
+
+        top = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.mainlogoanimation);
+        bottom  = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.sublogoanimation);
+
+        img1.setAnimation(top);
+        img2.setAnimation(bottom);
     }
 }
