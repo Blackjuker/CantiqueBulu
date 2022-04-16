97f337781b6715bc1739ebc1b733f4b1883781c6
Blackjuker <mikarno2@gmail.com>
2022-04-16 09:56:25 +0200

    splash layout

--- app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
+++ app/src/main/java/com/juker_enterprise/cantiquebulu/Splash.java
@@ -1,4 +1,14 @@
 package com.juker_enterprise.cantiquebulu;
 
-public class Splash {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
