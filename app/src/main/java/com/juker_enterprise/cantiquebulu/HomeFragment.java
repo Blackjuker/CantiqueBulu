cc09a27d07d2cd131ecaa867bd7ea57889d6313f
Blackjuker <mikarno2@gmail.com>
2022-04-25 12:27:06 +0200

    cantique load 6

--- app/src/main/java/com/juker_enterprise/cantiquebulu/HomeFragment.java
+++ app/src/main/java/com/juker_enterprise/cantiquebulu/HomeFragment.java
@@ -2,6 +2,7 @@ package com.juker_enterprise.cantiquebulu;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,35 +23,54 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     Cantique cantique ;
-    ArrayList listCantique = null;
-    Context context = getContext();
+    ArrayList <Cantique> listCantique = null;
+    Context mContext  ;
     String titre = null;
+    String name=null;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+          int resId ;
+        listCantique = new ArrayList();
+
+       try {
+           for (int i=1;i<=296;i++){
+               if(i<10){
+                   resId = getResources().getIdentifier("raw/b00"+String.valueOf(i),"raw",mContext.getPackageName());
+               }else if(i<100){
+                   resId = getResources().getIdentifier("raw/b0"+i,"raw",mContext.getPackageName());
+               }else{
+                   resId = getResources().getIdentifier("raw/b"+i,"raw",mContext.getPackageName());
+               }
+              // context.getResources().openRawResource(resId);
+               cantique = new Cantique();
+               cantique.setNumero(resId);
+               cantique.setCorps(getCorps(resId));
+               cantique.setTitre(titre);
+
+
+               listCantique.add(cantique);
+               titre = null;
+
+           }
+       }catch (Exception e){
+           Log.d("erreur2",e.getMessage());
+       }
+
+        Log.d("valeur2",String .valueOf(listCantique.get(295).getTitre()));
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
-        String name=null;
-        int resId ;
-        listCantique = new ArrayList();
-        for (int i=1;i<296;i++){
-            if(i<10){
-                resId = getResources().getIdentifier("raw/b00"+i,null,context.getPackageName());
-            }else if(i<100){
-                resId = getResources().getIdentifier("raw/b0"+i,null,context.getPackageName());
-            }else{
-                resId = getResources().getIdentifier("raw/b"+i,null,context.getPackageName());
-            }
-            cantique = new Cantique();
-            cantique.setNumero(resId);
-            cantique.setCorps(getCorps(resId));
-            cantique.setTitre(titre);
-
-
-            listCantique.add(cantique);
-            titre = null;
-
-        }
 
         return inflater.inflate(R.layout.fragment_home,container,false);
 
@@ -86,7 +106,7 @@ public class HomeFragment extends Fragment {
                 corpCantique = buf.toString();
             }
         } catch (IOException e) {
-            Toast.makeText(context, "FileNotFoundException", Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext , "FileNotFoundException", Toast.LENGTH_LONG).show();
             corpCantique = "FileNotFoundException " + e.getMessage();
         }
 
