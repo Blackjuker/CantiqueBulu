502aa75ab4e95f04033a10574e8f9536f8408fd0
Blackjuker <mikarno2@gmail.com>
2022-05-19 01:21:28 +0200

    affichage favoris refactor 3

--- app/src/main/java/com/hive_coorporation/cantiquebulu/InformationFragment.java
+++ app/src/main/java/com/hive_coorporation/cantiquebulu/InformationFragment.java
@@ -1,6 +1,8 @@
 package com.hive_coorporation.cantiquebulu;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +11,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
 public class InformationFragment extends Fragment {
+
+
+
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_apropos,container,false);
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
